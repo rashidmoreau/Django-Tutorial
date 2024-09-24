129d164688b3a5325c4be4a968041fe41f2c49f3
rashidmoreau <rashidmoreau@gmail.com>
2024-09-24 01:41:20 -0400

    Integrating Custom User Model

--- base/views.py
+++ base/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
@@ -23,14 +21,15 @@ def loginPage(request):
         return redirect('home')
     
     if request.method == 'POST':
+        email = request.POST.get('email').lower()
         username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
-           user = User.objects.get(username=username) 
+           user = User.objects.get(email=email) 
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
@@ -46,10 +45,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -177,12 +176,13 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
 
 def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
--- studybud/settings.py
+++ studybud/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 
     'rest_framework',
-    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'base.User'
@@ -124,11 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 #STATIC_ROOT =
 
